e72996e3b7face97565deba41ac97ef625c25159
mart00n <awmarton@gmail.com>
2015-04-01 13:43:47 -0400

    Test commit for SSH (gave credit to dev)

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 # SNAKES GAME
 # Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
+# Credit to sanchitgangwar for original program
 
 import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
